28aee6858c0e5e1c74f3935cb5c2ae3457b1bac8
Andreas Himmler <13476558+Andy0n@users.noreply.github.com>
2024-07-12 07:08:18 +0200

    nvim: java support

---
+++ nvim/.config/nvim/lua/andy/lazy/java.lua
@@ -0,0 +1,81 @@
+return {
+	{
+		"mfussenegger/nvim-jdtls",
+
+		dependencies = {
+			"neovim/nvim-lspconfig",
+		},
+
+		ft = { "java" },
+		enabled = true,
+
+		config = function()
+			print("jdtls loaded")
+			local cmp = require("cmp")
+			local cmp_lsp = require("cmp_nvim_lsp")
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				cmp_lsp.default_capabilities()
+			)
+
+			local jdtls = require("jdtls")
+			local jdtls_bin = vim.fn.stdpath("data") .. "/mason/bin/jdtls"
+
+			local root_markers = { ".gradle", "gradlew", ".git", "mvnw", "pom.xml" }
+			local root_dir = jdtls.setup.find_root(root_markers)
+			local home = os.getenv("HOME")
+			local project_name = vim.fn.fnamemodify(root_dir, ":p:h:t")
+			local workspace_dir = home .. "/.cache/jdtls/workspace/" .. project_name
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("lsp-java-group", {}),
+				pattern = "java",
+				callback = function(e)
+					vim.opt.tabstop = 2
+					vim.opt.softtabstop = 2
+					vim.opt.shiftwidth = 2
+					vim.opt.expandtab = true
+
+					jdtls.start_or_attach({
+						capabilities = capabilities,
+
+						cmd = {
+							jdtls_bin,
+							"-data",
+							workspace_dir,
+						},
+
+						root_dir = vim.fs.dirname(vim.fs.find(root_markers, { upward = true })[1]),
+						-- root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew" }),
+
+						on_attach = function(client, bufnr)
+							jdtls.setup.add_commands() -- important to ensure you can update configs when build is updated
+							-- if you setup DAP according to https://github.com/mfussenegger/nvim-jdtls#nvim-dap-configuration you can uncomment below
+							-- jdtls.setup_dap({ hotcodereplace = "auto" })
+							-- jdtls.dap.setup_dap_main_class_configs()
+
+							-- you may want to also run your generic on_attach() function used by your LSP config
+						end,
+
+						settings = {
+							java = {
+								sources = {
+									organizeImports = {
+										starThreshold = 9999,
+										staticStarThreshold = 9999,
+									},
+								},
+							},
+						},
+
+						init_options = {
+							bundles = {},
+						},
+					})
+				end,
+			})
+		end,
+	},
+}
